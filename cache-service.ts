273abbe9c68148e6816c72368fe4ab001bb61b53
Enes Ozer <enesozer.f@gmail.com>
2021-09-13 15:13:06 +0300

    --bugfix
    -- fixed security access 
    -- changed split operator for globally
    -- writed single function for bulk deletion, and now  allow only cache key 
    -- review static split operator.

--- cache-service.ts
+++ cache-service.ts
@@ -1,47 +1,52 @@
 export class CacheService{
   cacheName="cache";
-  cacheSplitOp=':';
-  cacheItems:{key:string,value:string}[]=[];
-  setCacheItem(id:number,key:string,value:string){
-    localStorage.setItem(key+":"+id.toString(),value);
+  cacheSplitOp='@';
+  private cacheItems:{key:string,value:string}[]=[];
+  setCacheItem(id:string,key:string,value:string){
+    localStorage.setItem(this.cacheKeyGenerator(key,id),value);
   }
-  getCacheItemByID(id:number,key:string):string{
+  getCacheItemByID(id:string,key:string):string{
     return localStorage.getItem(key+":"+id.toString());
   }
   compareCacheItem(CurrentId:number,CurrentKey:string,value:string):boolean{
-    return localStorage.getItem(CurrentKey+":"+CurrentId.toString())==value;
+    return localStorage.getItem(CurrentKey+this.cacheSplitOp+CurrentId.toString())==value;
   }
-
   getAllCacheByKey(key:string):any[]{
     this.cacheItems=[];
     for (let i = 0; i < localStorage.length; i++) {
-      if(localStorage.key(i).split(":")[0]==key){
+      if(localStorage.key(i).split(this.cacheSplitOp)[0]==key){
         this.cacheItems.push({key:localStorage.key(i),value:localStorage.getItem(localStorage.key(i))});
       }
     }
     return this.cacheItems;
   }
-  clearCacheByKey(key:string){
+  clearCache(key:string){
     for (let i = 0; i < localStorage.length; i++) {
-      if(localStorage.key(i).split(":")[0]==key){
+      if(localStorage.key(i).split(this.cacheSplitOp)[0]==key){
         localStorage.removeItem(localStorage.key(i));
       }
     }
   }
-  clearCache(){
-    localStorage.clear();
+
+  deleteItem(key:string,id:string):boolean{
+    let fullKey:string=this.cacheKeyGenerator(key,id);
+    if(localStorage.getItem(fullKey)){
+      localStorage.removeItem(fullKey);
+      return true;
+    }else{
+      return false;
+    }
   }
-  cacheKeyGenerator(key:string,id:number):string{
+  cacheKeyGenerator(key:string,id:string):string{
     let keyStringArr:string[]=[];
-    let keyString:string="";
+    let keyString:string="";//order: [cache-name]:[key]:[id]
     keyStringArr.push(this.cacheName);
     keyStringArr.push(key);
     keyStringArr.push(id.toString());
     for (let index = 0; index < keyStringArr.length-1; index++) {
-      keyString += keyStringArr[index]+this.cacheSplitOp;
+      keyString += keyStringArr[index]+this.cacheSplitOp;//generate key with split operator
     }
-    
-    keyString += keyStringArr[keyStringArr.length-1];
+    keyString += keyStringArr[keyStringArr.length-1];//for last item
     return keyString;
   }
 }
